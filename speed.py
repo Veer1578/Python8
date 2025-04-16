639d3e786fb028ae67465960b41e5cde04a4dcb6
Veer1578 <sweetyatulshah@gmail.com>
2025-04-16 09:36:35 +0530

    speed

---
+++ speed.py
@@ -0,0 +1,14 @@
+a = 10
+b = 20
+c = 30
+avg = (a + b + c)/3
+print("Average is ",avg)
+
+if avg > a:
+    print("a was cycling slower than average speed")
+elif avg > b:
+    print("b was cycling slower than average speed")
+elif avg > c:
+    print("c was cycling slower than average speed")
+else:
+    print("Invalid Output")
